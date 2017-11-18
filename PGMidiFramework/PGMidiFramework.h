87b8af76935bb52a3fb0e0db31d7e3864a566920
Alex Nadzharov <anadjarov@gmail.com>
2017-11-18 16:09:12 +0300

    ios framework target stub

---
+++ PGMidiFramework/PGMidiFramework.h
@@ -0,0 +1,19 @@
+//
+//  PGMidiFramework.h
+//  PGMidiFramework
+//
+//  Created by Alex on 18/11/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PGMidiFramework.
+FOUNDATION_EXPORT double PGMidiFrameworkVersionNumber;
+
+//! Project version string for PGMidiFramework.
+FOUNDATION_EXPORT const unsigned char PGMidiFrameworkVersionString[];
+
+#import "PGMidi.h"
+
+
+
